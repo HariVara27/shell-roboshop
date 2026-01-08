0bbb4d3af951db8d4e9d352ae7d322a8c0a6dbbd
Hari Vara <harivaraprasad7@yahoo.com>
2026-01-08 07:22:08 +0530

    SHell

--- roboshop.sh
+++ roboshop.sh
@@ -6,34 +6,21 @@ DOMAIN_NAME="harivara.fun"
 
 for instance in $@
 do
-     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name, Value=$instance}]' --query 'Instances[0].InstanceId' --output text)
-
-     if [ $instance != "frontend" ]; then
+     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+     
+      #get private IP
+     if [ "$instance" != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
         RECORD_NAME="$instance.$DOMAIN_NAME"
     else
          IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-
+          RECORD_NAME="$instance.$DOMAIN_NAME"
     fi
 
     echo "$instance: $IP"
 
-    aws route53 change-resource-record-sets \
+       aws route53 change-resource-record-sets \
   --hosted-zone-id $ZONE_ID \
-  --change-batch '
-  {
-    "Comment": "Updating record set"
-    ,"Changes": [{
-      "Action"              : "UPSERT"
-      ,"ResourceRecordSet"  : {
-        "Name"              : "'$RECORD_NAME'"
-        ,"Type"             : "A"
-        ,"TTL"              : 1
-        ,"ResourceRecords"  : [{
-            "Value"         : "'$IP'"
-        }]
-      }
-    }]
-  }
-  '
+  --change-batch "{\"Comment\":\"Updating record set\",\"Changes\":[{\"Action\":\"UPSERT\",\"ResourceRecordSet\":{\"Name\":\"$RECORD_NAME\",\"Type\":\"A\",\"TTL\":1,\"ResourceRecords\":[{\"Value\":\"$IP\"}]}}]}"
+
 done
